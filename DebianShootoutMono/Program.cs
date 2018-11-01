6b70144d35b73dd679c3ab091fd1c1bb0174874e
Alexander Kyte <alkyte@microsoft.com>
2018-11-01 16:08:48 -0400

    Make microbenchmark .exe easier to use for harness

--- DebianShootoutMono/Program.cs
+++ DebianShootoutMono/Program.cs
@@ -11,16 +11,50 @@ namespace BenchmarkDebianShootout
     {
         public static void Main(string[] args)
         {
-            BenchmarkRunner.Run<BinaryTrees>();
-            BenchmarkRunner.Run<Fannkuchredux>();
-            BenchmarkRunner.Run<Fasta>();
-            BenchmarkRunner.Run<KNucleotide>();
-            BenchmarkRunner.Run<Mandelbrot>();
-            BenchmarkRunner.Run<RegexRedux> ();
-            BenchmarkRunner.Run<RevComp>();
-            BenchmarkRunner.Run<SpectralNorm>();
-            BenchmarkRunner.Run<NBodyTest>();
-
+            if (args.Length == 0) {
+                BenchmarkRunner.Run<BinaryTrees>();
+                BenchmarkRunner.Run<Fannkuchredux>();
+                BenchmarkRunner.Run<Fasta>();
+                BenchmarkRunner.Run<KNucleotide>();
+                BenchmarkRunner.Run<Mandelbrot>();
+                BenchmarkRunner.Run<RegexRedux> ();
+                BenchmarkRunner.Run<RevComp>();
+                BenchmarkRunner.Run<SpectralNorm>();
+                BenchmarkRunner.Run<NBodyTest>();
+                return;
+            }
+            switch (args [0]) {
+                case "BinaryTrees":
+                    BenchmarkRunner.Run<BinaryTrees>();
+                    break;
+                case "Fannkuchredux":
+                    BenchmarkRunner.Run<Fannkuchredux>();
+                    break;
+                case "Fasta":
+                    BenchmarkRunner.Run<Fasta>();
+                    break;
+                case "KNucleotide":
+                    BenchmarkRunner.Run<KNucleotide>();
+                    break;
+                case "Mandelbrot":
+                    BenchmarkRunner.Run<Mandelbrot>();
+                    break;
+                case "RegexRedux":
+                    BenchmarkRunner.Run<RegexRedux>();
+                    break;
+                case "RevComp":
+                    BenchmarkRunner.Run<RevComp>();
+                    break;
+                case "SpectralNorm":
+                    BenchmarkRunner.Run<SpectralNorm>();
+                    break;
+                case "NBodyTest":
+                    BenchmarkRunner.Run<NBodyTest>();
+                    break;
+                default:
+                    Console.WriteLine ("No such microbenchmark");
+                    break;
+            }
         }
     }
 }
