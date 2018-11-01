05c9dae71fffeca73c40d936cb49e8b6845361b1
Alexander Kyte <alkyte@microsoft.com>
2018-11-01 14:25:27 -0400

    BenchmarkDotNet building tests using direct .dll reference

---
+++ DebianShootoutMono/Program.cs
@@ -0,0 +1,26 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+
+using System;
+
+namespace BenchmarkDebianShootout
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<BinaryTrees>();
+            BenchmarkRunner.Run<Fannkuchredux>();
+            BenchmarkRunner.Run<Fasta>();
+            BenchmarkRunner.Run<KNucleotide>();
+            BenchmarkRunner.Run<Mandelbrot>();
+            BenchmarkRunner.Run<RegexRedux> ();
+            BenchmarkRunner.Run<RevComp>();
+            BenchmarkRunner.Run<SpectralNorm>();
+            BenchmarkRunner.Run<NBodyTest>();
+
+        }
+    }
+}
---
+++ DebianShootoutMono/src/Microbenchmarks.cs
@@ -0,0 +1,30 @@
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Diagnosers;
+
+namespace BenchmarkDebianShootout
+{
+	public class BenchmarkDebianShootoutConfig : ManualConfig
+	{
+		public BenchmarkDebianShootoutConfig ()
+		{
+			var monoRuntimePath = System.Environment.GetEnvironmentVariable ("MONO_BENCH_EXECUTABLE");
+            var monoPathVal = System.Environment.GetEnvironmentVariable("MONO_BENCH_PATH");
+            var aot_args = System.Environment.GetEnvironmentVariable("MONO_BENCH_AOT_BUILD");
+            var aot_run_args = System.Environment.GetEnvironmentVariable("MONO_BENCH_AOT_RUN");
+            var jobName = string.Format("Mono At {0}", monoRuntimePath);
+
+            var job = Job.ShortRun;
+            job = job.With(new MonoRuntime(jobName, monoRuntimePath, aot_args, monoPathVal));
+
+            if (aot_run_args != null)
+                job = job.With(new[] { new MonoArgument(aot_run_args) });
+
+            Add(job);
+
+            // Add(DisassemblyDiagnoser.Create(new DisassemblyDiagnoserConfig(printIL: true, printAsm: true, printPrologAndEpilog: true, recursiveDepth: 0)));
+		}
+	}
+}
