33f941b3812b07c9a57415ece0091e2064c99709
Alexander Kyte <alkyte@microsoft.com>
2018-11-02 17:46:41 -0400

    Add release dir cleaning

--- build.sh
+++ build.sh
@@ -6,8 +6,14 @@ cd external/BenchmarkDotNet && bash build.sh --skiptests=true && cd -
 
 nuget restore
 
-rm -rf release
+rm -rf release build
+
+mkdir build
+
+msbuild /property:Configuration=Release /property:OutputPath=`pwd`/build DebianShootoutMono.sln
 
 mkdir release
 
-msbuild /property:Configuration=Release /property:OutputPath=`pwd`/release DebianShootoutMono.sln
+cd build && cp CommandLine.dll BenchmarkDotNet.dll DebianShootoutMono.exe Microsoft.CodeAnalysis.CSharp.dll Microsoft.CodeAnalysis.dll Microsoft.DotNet.InternalAbstractions.dll Microsoft.DotNet.PlatformAbstractions.dll System.Collections.Immutable.dll System.Numerics.Vectors.dll System.Reflection.Metadata.dll System.Reflection.TypeExtensions.dll System.Reflection.dll System.Runtime.CompilerServices.Unsafe.dll System.Runtime.Extensions.dll System.Runtime.InteropServices.dll System.Runtime.dll System.Threading.Tasks.Extensions.dll System.Threading.Thread.dll System.ValueTuple.dll ../release  && cd -
+
+
