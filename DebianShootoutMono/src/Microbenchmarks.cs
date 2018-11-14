8c9758f97e6fa2076aa5b40c6f50e2fe33f81ae0
Alexander Kyte <alkyte@microsoft.com>
2018-11-14 17:08:07 -0500

    Add support for running with nested profiler

--- DebianShootoutMono/src/Microbenchmarks.cs
+++ DebianShootoutMono/src/Microbenchmarks.cs
@@ -8,26 +8,40 @@ namespace BenchmarkDebianShootout
 {
 	public class BenchmarkDebianShootoutConfig : ManualConfig
 	{
+		void 
+		AddRunner (string jobName, string monoPathVal, string monoRuntimePath, string aotArgs, string aotRunArgs)
+		{
+			var job = Job.ShortRun;
+			job = job.With(new MonoRuntime(jobName, monoRuntimePath, aotArgs, monoPathVal));
+
+			if (aotRunArgs != null)
+				job = job.With(new[] { new MonoArgument(aotRunArgs) });
+
+			Add(job);
+		}
+
 		public BenchmarkDebianShootoutConfig ()
 		{
 			var monoRuntimePath = System.Environment.GetEnvironmentVariable ("MONO_BENCH_EXECUTABLE");
-            var monoPathVal = System.Environment.GetEnvironmentVariable("MONO_BENCH_PATH");
-            var aot_args = System.Environment.GetEnvironmentVariable("MONO_BENCH_AOT_BUILD");
-            var aot_run_args = System.Environment.GetEnvironmentVariable("MONO_BENCH_AOT_RUN");
-            var jobName = string.Format("Mono At {0}", monoRuntimePath);
+			var monoPathVal = System.Environment.GetEnvironmentVariable("MONO_BENCH_PATH");
+			var aotArgs = System.Environment.GetEnvironmentVariable("MONO_BENCH_AOT_BUILD");
+			var aotRunArgs = System.Environment.GetEnvironmentVariable("MONO_BENCH_AOT_RUN");
+			var profilerPrefix = System.Environment.GetEnvironmentVariable("MONO_BENCH_PROFILE_PREFIX");
+			var profilerOnly = System.Environment.GetEnvironmentVariable("MONO_BENCH_ONLY_PROFILED");
+			var jobName = string.Format("Mono At {0}", monoRuntimePath);
 
-            // Default to use llvm, mono falls back to not where applicable
-            Job.Default.With(Jit.Llvm);
+			// Default to use llvm, mono falls back to not where applicable
+			Job.Default.With(Jit.Llvm);
 
-            var job = Job.ShortRun;
-            job = job.With(new MonoRuntime(jobName, monoRuntimePath, aot_args, monoPathVal));
+			if (profilerOnly != null) {
+				// Add a job that's not using a native profiler
+				this.AddRunner (jobName, monoPathVal, monoRuntimePath, aotArgs, aotRunArgs);
+			}
 
-            if (aot_run_args != null)
-                job = job.With(new[] { new MonoArgument(aot_run_args) });
+			// Add a job that's using a native profiler
+			this.AddRunner (jobName, monoPathVal, profilerPrefix + monoRuntimePath, aotArgs, aotRunArgs);
 
-            Add(job);
-
-            // Add(DisassemblyDiagnoser.Create(new DisassemblyDiagnoserConfig(printIL: true, printAsm: true, printPrologAndEpilog: true, recursiveDepth: 0)));
+			// Add(DisassemblyDiagnoser.Create(new DisassemblyDiagnoserConfig(printIL: true, printAsm: true, printPrologAndEpilog: true, recursiveDepth: 0)));
 		}
 	}
 }
