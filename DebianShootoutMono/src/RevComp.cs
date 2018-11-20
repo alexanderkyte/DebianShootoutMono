e304134cb099c5c2215e9465fb07ee10668cc333
Alexander Kyte <alkyte@microsoft.com>
2018-11-20 17:15:58 -0500

    Add more RevComp cleanup

--- DebianShootoutMono/src/RevComp.cs
+++ DebianShootoutMono/src/RevComp.cs
@@ -22,16 +22,46 @@ using System.IO;
 
 namespace BenchmarkDebianShootout
 {
-	class RevCompSequence { public List<byte[]> Pages; public int StartHeader, EndExclusive; public Thread ReverseThread; }
+	public class RevCompSequence { public List<byte[]> Pages; public int StartHeader, EndExclusive; public Thread ReverseThread; }
 
 	[Config(typeof(BenchmarkDebianShootoutConfig))]
 	public class RevComp
 	{
+		[Benchmark]
+		public void Run ()
+		{
+			// Need object reference,
+			// otherwise we hide the initialization time of collections and stuff behind
+			// the static class init
+			new RevCompContainer ();
+		}
+	}
+
+	public class RevCompContainer
+	{
 		const int READER_BUFFER_SIZE = 1024 * 1024;
 		const byte LF = 10, GT = (byte)'>', SP = 32;
-		static BlockingCollection<byte[]> readQue = new BlockingCollection<byte[]>();
-		static BlockingCollection<RevCompSequence> writeQue = new BlockingCollection<RevCompSequence>();
-		static byte[] map;
+
+		public BlockingCollection<byte[]> readQue;
+		public BlockingCollection<RevCompSequence> writeQue;
+		public byte[] map;
+
+		Thread readerThread;
+		Thread grouperThread;
+
+		public RevCompContainer ()
+		{
+			readQue = new BlockingCollection<byte[]> ();
+			writeQue = new BlockingCollection<RevCompSequence> ();
+
+			readerThread = new Thread (this.Reader);
+			grouperThread = new Thread (this.Grouper);
+
+			readerThread.Start ();
+			grouperThread.Start ();
+
+			Writer ();
+		}
 
 		static int read(Stream stream, byte[] buffer, int offset, int count)
 		{
@@ -40,7 +70,8 @@ namespace BenchmarkDebianShootout
 				 : bytesRead==0 ? offset
 				 : read(stream, buffer, offset+bytesRead, count-bytesRead);
 		}
-		static void Reader()
+
+		public void Reader ()
 		{
 			using (var stream = File.Open (System.Environment.GetEnvironmentVariable ("MONO_BENCH_INPUT"), FileMode.Open))
 			{
@@ -49,9 +80,9 @@ namespace BenchmarkDebianShootout
 				{
 					var buffer = new byte[READER_BUFFER_SIZE];
 					bytesRead = read(stream, buffer, 0, READER_BUFFER_SIZE);
-					readQue.Add(buffer);
+					this.readQue.Add(buffer);
 				} while(bytesRead==READER_BUFFER_SIZE);
-				readQue.CompleteAdding();
+				this.readQue.CompleteAdding();
 			}
 		}
 	
@@ -62,42 +93,42 @@ namespace BenchmarkDebianShootout
 			return t!=null;
 		}
 	
-		static void Grouper()
+		public void Grouper ()
 		{
 			// Set up complements map
-			map = new byte[256];
-			for (byte b=0; b<255; b++) map[b]=b;
-			map[(byte)'A'] = (byte)'T';
-			map[(byte)'B'] = (byte)'V';
-			map[(byte)'C'] = (byte)'G';
-			map[(byte)'D'] = (byte)'H';
-			map[(byte)'G'] = (byte)'C';
-			map[(byte)'H'] = (byte)'D';
-			map[(byte)'K'] = (byte)'M';
-			map[(byte)'M'] = (byte)'K';
-			map[(byte)'R'] = (byte)'Y';
-			map[(byte)'T'] = (byte)'A';
-			map[(byte)'V'] = (byte)'B';
-			map[(byte)'Y'] = (byte)'R';
-			map[(byte)'a'] = (byte)'T';
-			map[(byte)'b'] = (byte)'V';
-			map[(byte)'c'] = (byte)'G';
-			map[(byte)'d'] = (byte)'H';
-			map[(byte)'g'] = (byte)'C';
-			map[(byte)'h'] = (byte)'D';
-			map[(byte)'k'] = (byte)'M';
-			map[(byte)'m'] = (byte)'K';
-			map[(byte)'r'] = (byte)'Y';
-			map[(byte)'t'] = (byte)'A';
-			map[(byte)'v'] = (byte)'B';
-			map[(byte)'y'] = (byte)'R';
+			this.map = new byte[256];
+			for (byte b=0; b<255; b++) this.map[b]=b;
+			this.map[(byte)'A'] = (byte)'T';
+			this.map[(byte)'B'] = (byte)'V';
+			this.map[(byte)'C'] = (byte)'G';
+			this.map[(byte)'D'] = (byte)'H';
+			this.map[(byte)'G'] = (byte)'C';
+			this.map[(byte)'H'] = (byte)'D';
+			this.map[(byte)'K'] = (byte)'M';
+			this.map[(byte)'M'] = (byte)'K';
+			this.map[(byte)'R'] = (byte)'Y';
+			this.map[(byte)'T'] = (byte)'A';
+			this.map[(byte)'V'] = (byte)'B';
+			this.map[(byte)'Y'] = (byte)'R';
+			this.map[(byte)'a'] = (byte)'T';
+			this.map[(byte)'b'] = (byte)'V';
+			this.map[(byte)'c'] = (byte)'G';
+			this.map[(byte)'d'] = (byte)'H';
+			this.map[(byte)'g'] = (byte)'C';
+			this.map[(byte)'h'] = (byte)'D';
+			this.map[(byte)'k'] = (byte)'M';
+			this.map[(byte)'m'] = (byte)'K';
+			this.map[(byte)'r'] = (byte)'Y';
+			this.map[(byte)'t'] = (byte)'A';
+			this.map[(byte)'v'] = (byte)'B';
+			this.map[(byte)'y'] = (byte)'R';
 	
 			var startHeader = 0;
 			var i = 0;
 			bool afterFirst = false;
 			var data = new List<byte[]>();
 			byte[] bytes;
-			while (tryTake(readQue, out bytes))
+			while (tryTake(this.readQue, out bytes))
 			{
 				data.Add(bytes);
 				while((i=Array.IndexOf<byte>(bytes, GT, i+1))!=-1)
@@ -108,7 +139,7 @@ namespace BenchmarkDebianShootout
 						(sequence.ReverseThread = new Thread(() => Reverse(sequence))).Start();
 					else
 						afterFirst = true;
-					writeQue.Add(sequence);
+					this.writeQue.Add(sequence);
 					startHeader = i;
 					data = new List<byte[]> { bytes };
 				}
@@ -117,11 +148,11 @@ namespace BenchmarkDebianShootout
 			var lastSequence = new RevCompSequence { Pages = data
 				, StartHeader = startHeader, EndExclusive = i==-1 ? data[data.Count-1].Length : i };
 			Reverse(lastSequence);
-			writeQue.Add(lastSequence);
-			writeQue.CompleteAdding();
+			this.writeQue.Add(lastSequence);
+			this.writeQue.CompleteAdding();
 		}
 	
-		static void Reverse(RevCompSequence sequence)
+		void Reverse(RevCompSequence sequence)
 		{
 			var startPageId = 0;
 			var startBytes = sequence.Pages[0];
@@ -182,7 +213,7 @@ namespace BenchmarkDebianShootout
 			if (startIndex == endIndex) startBytes[startIndex] = map[startBytes[startIndex]];
 		}
 	
-		static void Writer()
+		void Writer ()
 		{
 			var outName = String.Format("{0}.out", System.Environment.GetEnvironmentVariable ("MONO_BENCH_INPUT"));
 
@@ -190,7 +221,7 @@ namespace BenchmarkDebianShootout
 			{
 				bool first = true;
 				RevCompSequence sequence;
-				while (tryTake(writeQue, out sequence))
+				while (tryTake(this.writeQue, out sequence))
 				{
 					var startIndex = sequence.StartHeader;
 					var pages = sequence.Pages;
@@ -212,14 +243,6 @@ namespace BenchmarkDebianShootout
 					stream.Write(pages[pages.Count-1], startIndex, sequence.EndExclusive - startIndex);
 				}
 			}
-		}
-
-		[Benchmark]
-		public void Run ()
-		{
-			new Thread(Reader).Start();
-			new Thread(Grouper).Start();
-			Writer();
 		}
 	}
 }
